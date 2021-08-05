507bab8adc73cb30597416d8d4e50834702a19c2
Guna <gunagang24@gmail.com>
2021-08-05 14:24:31 +0530

    fully re modified

--- quiz/serializers.py
+++ quiz/serializers.py
@@ -2,12 +2,14 @@ from django.db.models import fields
 from rest_framework import serializers
 from . import models
 
-class ChoiceSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = models.Category
-		fields = '__all__'
-
 class QuestionSerializer(serializers.ModelSerializer):
+	
 	class Meta:
 		model = models.Question
+		fields = '__all__'
+
+class CategorySerializer(serializers.ModelSerializer):
+	question = QuestionSerializer(read_only=True, many=True)
+	class Meta:
+		model = models.Category
 		fields = '__all__'
--- quiz/views.py
+++ quiz/views.py
@@ -6,15 +6,9 @@ from . import serializers
 def home(request):
 	return render(request, 'quiz/home.html')
 
-def categories(request):
-	return render(request, 'quiz/categories.html')
-
-def question(request):
-	return render(request, 'quiz/question.html')
-
 class CategoryViewSet(viewsets.ModelViewSet):
 	queryset = models.Category.objects.all()
-	serializer_class = serializers.ChoiceSerializer
+	serializer_class = serializers.CategorySerializer
 
 class QuestionViewSet(viewsets.ModelViewSet):
 	queryset = models.Question.objects.all()
